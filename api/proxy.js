bf2090948fc33037422f16cf5e0ee388fb1295c4
gonzaloogv23 <gonzalogomezvignudo@gmail.com>
2024-11-24 18:05:18 -0300

    xd

--- api/proxy.js
+++ api/proxy.js
@@ -4,24 +4,24 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  next();
+ res.header('Access-Control-Allow-Origin', '*');
+ res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+ res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+ next();
 });
 
 app.options('*', (req, res) => {
-  res.status(200).send();
+ res.status(200).send();
 });
 
 const apiProxy = createProxyMiddleware({
-  target: 'https://api.sambanova.ai/v1',
-  changeOrigin: true,
-  pathRewrite: {
-    '^/api': ''
-  }
+ target: 'https://api.sambanova.ai/v1',
+ changeOrigin: true,
+ pathRewrite: { '^/v1': '' },
 });
 
-app.use('/api', apiProxy);
+app.use('/v1', apiProxy);
 
-module.exports = app;
+app.listen(3002, () => {
+ console.log('Proxy escuchando en el puerto 3002');
+});

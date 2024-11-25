acfd307b3fdd51591275b2577b245dd5acedde1c
gonzaloogv23 <gonzalogomezvignudo@gmail.com>
2024-11-25 03:12:13 -0300

    Update EnviarCuestionario.js

--- src/components/EnviarCuestionario.js
+++ src/components/EnviarCuestionario.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import SambanovaService from '../services/SambanovaService';
+import * as pdfjsLib from 'pdfjs-dist/webpack';
+
+// Configurar el worker para pdf.js
+pdfjsLib.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@${pdfjsLib.version}/build/pdf.worker.min.js`;
 
 const EnviarCuestionario = () => {
   const [cuestionario, setCuestionario] = useState(null);
@@ -7,17 +11,18 @@ const EnviarCuestionario = () => {
 
   const handleSeleccionarCuestionario = (e) => {
     setCuestionario(e.target.files[0]);
-    console.log("cuestionario: ", cuestionario)
+    console.log("cuestionario: ", e.target.files[0]);
   };
 
   const handleEnviarCuestionario = async () => {
     if (cuestionario) {
       try {
-        const respuesta = await SambanovaService.enviarCuestionario(cuestionario);
+        const textoExtraido = await extractTextFromPDF(cuestionario);
+        const respuesta = await SambanovaService.enviarCuestionario(textoExtraido); // Asegúrate de enviar el texto extraído
         if (respuesta.error) {
           setRespuesta(respuesta.error);
         } else {
-          setRespuesta(respuesta);
+          setRespuesta(respuesta.message); // Acceder al mensaje de la respuesta transformada
         }
       } catch (error) {
         console.error("Error al enviar el cuestionario", error);
@@ -25,7 +30,30 @@ const EnviarCuestionario = () => {
       }
     }
   };
-  
+
+  const extractTextFromPDF = async (file) => {
+    const arrayBuffer = await file.arrayBuffer();
+
+    // Cargar el documento PDF
+    const pdf = await pdfjsLib.getDocument(arrayBuffer).promise;
+
+    let extractedText = '';
+
+    // Recorrer las páginas del PDF
+    for (let i = 0; i < pdf.numPages; i++) {
+      const page = await pdf.getPage(i + 1); // las páginas se indexan desde 1
+
+      // Obtener el contenido de la página
+      const content = await page.getTextContent();
+
+      // Extraer el texto de los items
+      const pageText = content.items.map(item => item.str).join(' ');
+      extractedText += pageText + '\n'; // Añadir un salto de línea entre páginas
+    }
+    console.log(extractedText)
+    return extractedText;
+    
+  };
 
   return (
     <div>

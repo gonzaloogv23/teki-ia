ded1569c5a0a21bafb0f7bbe65064f2c115c0125
gonzaloogv23 <gonzalogomezvignudo@gmail.com>
2024-11-24 21:19:11 -0300

    plis bro

--- scripts/extract_and_store.js
+++ scripts/extract_and_store.js
@@ -1,26 +1,15 @@
 const fs = require('fs');
 const pdf = require('pdf-parse');
-const sqlite3 = require('sqlite3').verbose();
+const { initializeApp } = require('firebase/app');
+const { getFirestore, collection, addDoc, serverTimestamp } = require('firebase/firestore');
 const path = require('path');
 
-const dbPath = path.resolve(__dirname,'.','server', 'db', 'mi_base_de_datos.db');
-const db = new sqlite3.Database(dbPath, (err) => {
-    if (err) {
-        console.error('Error al conectar con la base de datos', err);
-        // Crear la base de datos si no existe
-        fs.mkdirSync(path.dirname(dbPath), { recursive: true });
-        const db = new sqlite3.Database(dbPath);
-        db.run(`CREATE TABLE IF NOT EXISTS conocimiento (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            de_contenido TEXT NOT NULL,
-            fe_timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-        )`);
-    } else {
-        console.log('Conectado a la base de datos SQLite');
-    }
-});
+// Importar la configuración de Firebase
+const firebaseConfig = require('./firebaseConfig');
 
-
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 const extractTextFromPDF = async (filePath) => {
     const dataBuffer = fs.readFileSync(filePath);
@@ -28,21 +17,23 @@ const extractTextFromPDF = async (filePath) => {
     return data.text;
 };
 
-const storeKnowledgeInDatabase = (contenido) => {
-    db.run('INSERT INTO conocimiento (de_contenido) VALUES (?)', [contenido], function(err) {
-        if (err) {
-            console.error('Error al insertar conocimiento en la base de datos', err);
-        } else {
-            console.log('Conocimiento almacenado con éxito');
-        }
-    });
+const storeKnowledgeInDatabase = async (contenido) => {
+    try {
+        await addDoc(collection(db, 'conocimiento'), {
+            de_contenido: contenido,
+            fe_timestamp: serverTimestamp()
+        });
+        console.log('Conocimiento almacenado con éxito en Firestore');
+    } catch (error) {
+        console.error('Error al insertar conocimiento en Firestore', error);
+    }
 };
 
 // Procesar y almacenar contenido del PDF
 const processAndStorePDF = async (filePath) => {
     try {
         const contenido = await extractTextFromPDF(filePath);
-        storeKnowledgeInDatabase(contenido);
+        await storeKnowledgeInDatabase(contenido);
     } catch (error) {
         console.error('Error al procesar y almacenar el PDF', error);
     }
